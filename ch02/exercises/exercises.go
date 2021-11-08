87d8824a2de9f71a5e249b6b619c216eca2c8360
berny linville <bernylinville@devopsthink.org>
2021-11-08 15:39:50 +0800

    update ch02 exercise

---
+++ ch02/exercises/exercises.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func ats(a1, a2 []string) []string {
+	return append(a1, a2...)
+}
+
+func main() {
+	a1 := [...]string{"a1", "a2", "a3"}
+	a2 := [...]string{"b1", "b2", "b3"}
+	ats := ats(a1[:], a2[:])
+	fmt.Println(ats)
+	fmt.Println(append(a1, a2))
+}
