9223512bafb45fe996a9f4ca3c5ad7d0e975b14e
berny linville <bernylinville@devopsthink.org>
2021-11-19 17:12:07 +0800

    update ch06 go-cobra

---
+++ ch06/go-cobra/cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// A Global variable
+var Special = "This is a special message."
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "go-cobra",
+	Short: "A brief description of your application",
+	Long:  `A longer description that spans multiple lines and likely contains`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringP("directory", "d", "/tmp", "Path to use.")
+	rootCmd.PersistentFlags().Uint("depth", 2, "Depth of search")
+
+	viper.BindPFlag("directory", rootCmd.PersistentFlags().Lookup("directory"))
+	viper.BindPFlag("depth", rootCmd.PersistentFlags().Lookup("depth"))
+}
