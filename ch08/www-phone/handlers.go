a7d46562f0058dd53d2de2b37f6a36a686afb9a7
Berny Linville <bernylinville@devopsthink.org>
2021-12-03 10:17:11 +0800

    update handlers.go

--- ch08/www-phone/handlers.go
+++ ch08/www-phone/handlers.go
@@ -1,3 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
 const PORT = ":1234"
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving request:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Thanks for visiting!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	// Get telephone
+	paramStr := strings.Split(r.URL.Path, "/")
+
+	fmt.Println("Path:", paramStr)
+	if len(paramStr) < 3 {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Not found"+r.URL.Path)
+		return
+	}
+
+	log.Println("Serving request:", r.URL.Path, "from", r.Host)
+	telephone := paramStr[2]
+	err := deleteEntry(telephone)
+	if err != nil {
+		fmt.Println(err)
+		Body := err.Error() + "\n"
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s", Body)
+		return
+	}
+
+	Body := telephone + " deleted!\n"
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", Body)
+}
