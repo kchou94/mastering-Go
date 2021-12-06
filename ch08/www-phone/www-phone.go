5127676c5ae54c1cdc90095c8017f2ddaf5db139
Berny Linville <bernylinville@devopsthink.org>
2021-12-06 11:43:52 +0800

    update www-phone

--- ch08/www-phone/www-phone.go
+++ ch08/www-phone/www-phone.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
+	"net/http"
 	"os"
+	"regexp"
+	"strconv"
+	"time"
 )
 
 type Entry struct {
@@ -50,4 +55,140 @@ func readCSVFile(filepath string) error {
 	}
 
 	return nil
+}
+
+func saveCSVFile(filepath string) error {
+	csvfile, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer csvfile.Close()
+
+	csvwriter := csv.NewWriter(csvfile)
+	for _, row := range data {
+		temp := []string{row.Name, row.Surname, row.Tel, row.LastAccess}
+		_ = csvwriter.Write(temp)
+	}
+	csvwriter.Flush()
+	return nil
+}
+
+func createIndex() error {
+	index = make(map[string]int)
+	for i, k := range data {
+		key := k.Tel
+		index[key] = i
+	}
+	return nil
+}
+
+// Initialized by the user – returns a pointer
+// If it returns nil, there was an error
+func initS(N, S, T string) *Entry {
+	// Both of them should have a value
+	if T == "" || S == "" {
+		return nil
+	}
+	// Give LastAccess a value
+	LastAccess := strconv.FormatInt(time.Now().Unix(), 10)
+	return &Entry{N, S, T, LastAccess}
+}
+
+func insert(pS *Entry) error {
+	// If it already exists, do not add it
+	_, ok := index[(*pS).Tel]
+	if ok {
+		return fmt.Errorf("%s already exists", (*pS).Tel)
+	}
+
+	*&pS.LastAccess = strconv.FormatInt(time.Now().Unix(), 10)
+	data = append(data, *pS)
+	// Update the index
+	_ = createIndex()
+
+	err := saveCSVFile(CSVFILE)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func deleteEntry(key string) error {
+	i, ok := index[key]
+	if !ok {
+		return fmt.Errorf("%s not found", key)
+	}
+	data = append(data[:i], data[i+1:]...)
+	// Update the index
+	delete(index, key)
+
+	err := saveCSVFile(CSVFILE)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func search(key string) *Entry {
+	i, ok := index[key]
+	if !ok {
+		return nil
+	}
+	data[i].LastAccess = strconv.FormatInt(time.Now().Unix(), 10)
+	return &data[i]
+}
+
+func matchTel(s string) bool {
+	t := []byte(s)
+	re := regexp.MustCompile(`\d+$`)
+	return re.Match(t)
+}
+
+func list() string {
+	var all string
+	for _, k := range data {
+		all += k.Name + " " + k.Surname + " " + k.Tel + "\n"
+	}
+	return all
+}
+
+func main() {
+	err := readCSVFile(CSVFILE)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = createIndex()
+	if err != nil {
+		fmt.Println("Cannot create index.")
+		return
+	}
+
+	mux := http.NewServeMux()
+	s := &http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		IdleTimeout:  time.Second * 10,
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+
+	mux.handle("/list", http.HandlerFunc(listHandler))
+	mux.Handle("/insert/", http.HandlerFunc(insertHandler))
+	mux.Handle("/insert", http.HandlerFunc(insertHandler))
+	mux.Handle("/search", http.HandlerFunc(searchHandler))
+	mux.Handle("/search/", http.HandlerFunc(searchHandler))
+	mux.Handle("/delete/", http.HandlerFunc(deleteHandler))
+	mux.Handle("/status", http.HandlerFunc(statusHandler))
+	mux.Handle("/", http.HandlerFunc(defaultHandler))
+
+	fmt.Println("Ready to serve at", PORT)
+	err = s.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
